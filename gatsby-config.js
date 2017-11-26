2f9dc2aefd161fe5e924faddf656175ca270ee6b
Kevin Sherman <kevin@kshermphoto.com>
2017-11-26 07:12:34 -0600

    Adds remark plugin to config

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,14 @@ module.exports = {
       },
     },
     {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'posts',
+        path: `${__dirname}/src/posts/`,
+      },
+    },
+    'gatsby-transformer-remark',
+    {
       resolve: 'gatsby-plugin-google-fonts',
       options: {
         fonts: ['Didact Gothic', 'Arimo'],
