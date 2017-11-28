6012461de8df0515f2feca1036ce5623bd81a995
Kevin Sherman <kevin@kshermphoto.com>
2017-11-28 02:58:59 -0600

    Updates

--- path---about-a33c1f4bfb6af2d2436a.js
+++ path---about-a33c1f4bfb6af2d2436a.js
@@ -1,2 +1,2 @@
-webpackJsonp([0xf927f8900006],{338:function(t,e){t.exports={data:{site:{siteMetadata:{title:"KDSherm"}}},pathContext:{}}}});
+webpackJsonp([0xf927f8900006],{339:function(t,e){t.exports={data:{site:{siteMetadata:{title:"KDSherm"}}},pathContext:{}}}});
 //# sourceMappingURL=path---about-a33c1f4bfb6af2d2436a.js.map
